816b126a50f463a3ff3aeebeff3f3e12be3d6a5d
sacha001 <ssilance@unb.ca>
2018-02-15 14:16:02 -0400

    done part 2

--- A3/main.c
+++ A3/main.c
@@ -27,12 +27,50 @@ void getWordStats(char *word, FILE *f, int *sum, int *num){
 }
 
 int main(){
-  char *word = "organic";
+
+
   FILE *file;
-  file = fopen("reviews.txt", "r");
   int num = 0;
   int sum = 0;
+  /* part1\
+  char word[200];
+  printf("Enter a word: ");
+  scanf("%s", word);
   getWordStats(word, file, &sum, &num);
   fclose(file);
-  printf("%d %d\n", num, sum);
+  printf("%s appeared %d times\n", word, num);
+  double avg = (double)sum/(double)num;
+  printf("The average score for reviews containing %s is %lf\n",word, avg);
+  */
+  printf("Enter a review\n");
+  char review[1000];
+  fgets(review, 1000, stdin);
+  review[strlen(review)-1] = '\0';
+
+  const char s[2] = " ";
+   char *token;
+
+   token = strtok(review, s);
+
+   while( token != NULL ) {
+     file = fopen("reviews.txt", "r");
+
+      getWordStats(token, file, &sum, &num);
+      int numTotal = 0;
+      double avgTotal = 0;
+      double avg = 0;
+      if(sum > 0){
+        avgTotal += (double)sum/(double)num;
+        numTotal++;
+        if(numTotal > 0){
+         avg = avgTotal/numTotal;
+      }
+
+      }
+
+      printf("%lf\n", avg);
+      token = strtok(NULL, s);
+      fclose(file);
+   }
+
 }
